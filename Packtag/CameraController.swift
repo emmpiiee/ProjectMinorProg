2d0b86b85d9a3c2fc2022d7269d07da8cc2b82e3
emmpiiee <emmaimmink@hotmail.com>
2016-06-08 16:56:48 +0200

    photo controller is working

--- Packtag/CameraController.swift
+++ Packtag/CameraController.swift
@@ -8,4 +8,34 @@
 
 import UIKit
 
-class CameraController 
+class CameraController:UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var selectedImageView: UIImageView!
+    @IBOutlet weak var sourceLabel: UILabel!
+    var selectedImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sourceLabel.text = "No image selected"
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        selectedImage = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.selectedImageView.image = selectedImage
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func takePhoto(sender: UIButton!){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.sourceType = .Camera
+        picker.delegate = self
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    @IBAction func selectPhoto(sender: UIButton!){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.sourceType = .PhotoLibrary
+        picker.delegate = self
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+}
