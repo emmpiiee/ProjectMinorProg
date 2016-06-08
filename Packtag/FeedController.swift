88e9d4d980427fff8dd9564c03912df449d282c0
emmpiiee <emmaimmink@hotmail.com>
2016-06-08 12:07:31 +0200

    feed controller the 2 requirements almost finisht

--- Packtag/FeedController.swift
+++ Packtag/FeedController.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class FeedController: UITableViewController {
     
+    // every section needs only 1 row for only 1 post
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // every post needs to be in a different section so return number of posts
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let feed = Post.feed{
+            return feed.count
+        }
+        else {
+            return 0
+        }
+    }
+    
+    // make sure latest post is first
+    func postIndex(cellIndex : Int) -> Int {
+        return tableView.numberOfSections - cellIndex - 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let post = Post.feed![postIndex(indexPath.section)]
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+        }
 }
