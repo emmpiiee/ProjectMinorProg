3229f6992a930354e8e64a210639fb30986dda45
emmpiiee <emmaimmink@hotmail.com>
2016-06-08 10:06:08 +0200

    start feeds controller

---
+++ Packtag/FeedController.swift
@@ -0,0 +1,13 @@
+//
+//  FeedController.swift
+//  Packtag
+//
+//  Created by Emma Immink on 07-06-16.
+//  Copyright © 2016 Emma Immink. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UITableViewController {
+    
+}
