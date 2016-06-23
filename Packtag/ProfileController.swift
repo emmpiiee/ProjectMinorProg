aeefd2ba8daf84a516bec71050031fe2b549abfb
emmpiiee <emmaimmink@hotmail.com>
2016-06-23 22:14:01 +0200

    Profile controller commented

--- Packtag/ProfileController.swift
+++ Packtag/ProfileController.swift
@@ -10,9 +10,9 @@ import UIKit
 import SwiftyDropbox
 
 class ProfileController: UIViewController {
+    // Create outlets and variables.
     @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var editProfile: UIButton!
-    
     @IBAction func editProfile(sender: AnyObject){
         print("users want to edit profile")
     }
@@ -24,13 +24,15 @@ class ProfileController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Show edit if current user same as profileUser.
         if (TodoManager.sharedInstance.userId == TodoManager.sharedInstance.profileViewId || TodoManager.sharedInstance.profileViewId == ""){
-            print("dit is de gelijk aan: user id \(TodoManager.sharedInstance.userId) en profileviewid \(TodoManager.sharedInstance.profileViewId)")
+            return
         }
+            // Don't show edit if current user and profile user are different.
         else {
             editProfile.hidden = true
-            print("dit is ongelijk aan aan: user id \(TodoManager.sharedInstance.userId) en profileviewid \(TodoManager.sharedInstance.profileViewId)")
         }
+        // If no profileViewId then current user is profileViewID.
         if (TodoManager.sharedInstance.profileViewId == "") {
             TodoManager.sharedInstance.profileViewId = TodoManager.sharedInstance.userId
         }
@@ -39,6 +41,7 @@ class ProfileController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        // If view appears, reload profile.
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ProfileController.reloadProfile), name: "reloadProfile", object: nil)
         if (TodoManager.sharedInstance.lastShowedProfileId == TodoManager.sharedInstance.profileViewId) {
             return
@@ -48,15 +51,12 @@ class ProfileController: UIViewController {
             if let client = Dropbox.authorizedClient {
                 // List folder
                 client.files.listFolder(path: "\(TodoManager.sharedInstance.path)").response { response, error in
-                    print("*** List folder ***")
                     if let result = response {
                         for entry in result.entries {
                             let subString = self.getStringsBeforeCharacter(entry.name, character: "`")
                             if(subString.count == 2) {
                                 filenames?.append(entry.name)
-                                print("dit is de array filenames \(filenames)")
                                 // download a file
-                                
                                 let destination : (NSURL, NSHTTPURLResponse) -> NSURL = { temporaryURL, response in
                                     let fileManager = NSFileManager.defaultManager()
                                     let directoryURL = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
@@ -65,15 +65,12 @@ class ProfileController: UIViewController {
                                     let pathComponent = "\(UUID)-\(response.suggestedFilename!)"
                                     return directoryURL.URLByAppendingPathComponent(pathComponent)
                                 }
+                                // Download files from filenames.
                                 client.files.download(path: "\(TodoManager.sharedInstance.path)/\(entry.name)", destination: destination).response { response, error in
                                     if let (metadata, url) = response {
-                                        print("*** Download file ***")
-                                        print("het is 4 \(subString.count)")
                                         if (subString[0] == TodoManager.sharedInstance.profileViewId){
                                             let data = NSData(contentsOfURL: url)
                                             let picture = UIImage (data: data!)
-                                            print("Downloaded file name: \(metadata.name)")
-                                            print("Downloaded file url: \(url)")
                                             self.fileImage = picture!
                                             self.profilePic.image = self.fileImage
                                             TodoManager.sharedInstance.profileViewId = ""
@@ -91,12 +88,13 @@ class ProfileController: UIViewController {
         }
     }
     
-    // func to get array of strings before certain character
+    // Function to get array of strings before certain character.
     func getStringsBeforeCharacter (string: String, character: String) -> Array<String> {
         let subStringBefore = (string.componentsSeparatedByString(character))
         return subStringBefore
     }
     
+    // function to reload profile.
     func reloadProfile (){
         viewDidLoad()
         viewDidAppear(true)
