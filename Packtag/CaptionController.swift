dc54aee05fcd4b6d54ae1bc7d72a76cbefb9be4f
emmpiiee <emmaimmink@hotmail.com>
2016-06-23 21:48:05 +0200

    caption controller commented

--- Packtag/CaptionController.swift
+++ Packtag/CaptionController.swift
@@ -10,60 +10,44 @@ import UIKit
 import SwiftyDropbox
 
 class CaptionController: UIViewController {
+    // Create outlets and variables.
     @IBOutlet weak var captionText: UITextField!
     @IBOutlet weak var imagePreview: UIImageView!
     @IBOutlet weak var shareButton: UIButton!
     var selectedImage: UIImage!
     
-    
+    // If submitPressed upload photos to dropbox and return to feed.
     @IBAction func submitPressed (sender: UIButton){
-        
+        // If caption text is longer then 30 characters use only first 30
         if (captionText.text!.characters.count >= 30){
             captionText.text! = captionText.text!.substringToIndex(captionText.text!.startIndex.advancedBy(30))
         }
-        
+        // Convert to NSDAta.
         let imageData: NSData = UIImagePNGRepresentation(selectedImage!)!
-        
         if let client = Dropbox.authorizedClient {
-            print("komt tie hierin dan")
-            
+            // Create post with in name all information and a timestamp.
             client.files.upload(path: "\(TodoManager.sharedInstance.path)/\(TodoManager.sharedInstance.userName)`\(captionText.text!)`\(TodoManager.sharedInstance.userId)`likes`\(NSDate())`.jpg", body: imageData).response { response, error in
-                if let metadata = response {
-                    print("*** Upload file ****")
-                    print("Uploaded file name: \(metadata.name)")
-                    print("Uploaded file revision: \(metadata.rev)")
-                }
                 if let errorfile = error {
                     print(errorfile)
                 }
             }
         }
-        
+        // Clears image at cameracontroller and goes back to feedController.
         NSNotificationCenter.defaultCenter().postNotificationName("clearImage", object: nil)
-        NSNotificationCenter.defaultCenter().postNotificationName("reloadTable", object: nil)
-        
         let tabBarController = self.presentingViewController as? UITabBarController
         tabBarController!.selectedIndex = 0
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    // If backButtonPressed dismiss current viewcontroller.
     @IBAction func backButtonPressed (sender: UIButton){
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    // If not on keyboard tapped, dismiss keyboard.
     @IBAction func tap (sender: UITapGestureRecognizer!){
         captionText.resignFirstResponder()
     }
-    
-//    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-//        
-//        let currentCharacterCount = textField.text?.characters.count ?? 0
-//        if (range.length + range.location > currentCharacterCount){
-//            return false
-//        }
-//        let newLength = currentCharacterCount + string.characters.count - range.length
-//        return newLength <= 25
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
